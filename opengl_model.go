e414dd4dcfb5729c16418fe703b0f96ac9fb18a2
Joel Hays <jhays@blizzard.com>
2018-09-18 09:29:48 -0700

    cleanup

--- opengl_model.go
+++ opengl_model.go
@@ -32,7 +32,6 @@ func NewOpenGlModelRenderer(thing *jk.Thing, template *jk.Template, object *jk.J
 func (r *OpenGlModelRenderer) Render() {
 	model := mgl32.Ident4()
 	if r.thing != nil {
-		// scale := mgl32.Scale3D(float32(r.template.Size), float32(r.template.Size), float32(r.template.Size))
 		rotateX := mgl32.HomogRotate3DX(mgl32.DegToRad(float32(r.thing.Pitch)))
 		rotateY := mgl32.HomogRotate3DY(mgl32.DegToRad(float32(r.thing.Roll)))
 		rotateZ := mgl32.HomogRotate3DZ(mgl32.DegToRad(float32(r.thing.Yaw)))
