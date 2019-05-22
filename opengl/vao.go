d0ad9b5b6388196f9a1c9482b890e8ce386a0315
Joel Hays <jhays@blizzard.com>
2019-05-21 17:12:18 -0700

    format

--- opengl/vao.go
+++ opengl/vao.go
@@ -14,16 +14,20 @@ func LoadToVAO(data []float32) uint32 {
 	gl.GenVertexArrays(1, &vao)
 	gl.BindVertexArray(vao)
 
+	/* position */
 	gl.EnableVertexAttribArray(0)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
 	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 9*4, gl.PtrOffset(0))
 
+	/* normal */
 	gl.EnableVertexAttribArray(1)
 	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 9*4, gl.PtrOffset(3*4))
 
+	/* uv */
 	gl.EnableVertexAttribArray(2)
 	gl.VertexAttribPointer(2, 2, gl.FLOAT, false, 9*4, gl.PtrOffset(6*4))
 
+	/* light intensity */
 	gl.EnableVertexAttribArray(3)
 	gl.VertexAttribPointer(3, 1, gl.FLOAT, false, 9*4, gl.PtrOffset(7*4))
 
--- shader.go
+++ shader.go
@@ -33,10 +33,10 @@ func compileShader(source string, shaderType uint32) (uint32, error) {
 		var logLength int32
 		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
 
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+		shaderLog := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(shaderLog))
 
-		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+		return 0, fmt.Errorf("failed to compile %v: %v", source, shaderLog)
 	}
 
 	return shader, nil
